6e62d5f90b8ccc90e9c596a44f0e1784cba200ed
Joshua <thejoshua@hotmail.com>
2017-11-25 12:54:47 -0600

    Dashboard work with sorting by requests then name

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
     def home
-
+        @tenants = Tenant.all.order('request_count DESC NULLS LAST, name ASC');
+        @users = User.all
+        @questions = Question.all
+        @answers = Answer.all
     end
 end
