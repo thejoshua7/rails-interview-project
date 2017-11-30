4949b3a93bb0385f2f792c48d95d88b6fd0afce4
Joshua <thejoshua@hotmail.com>
2017-11-29 22:40:43 -0600

    Last minute notes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
     def home
+        # I could have just used the .count but I wanted the full collection 
+        # so I could build out display tables and dynamic links for testing the API
         @tenants = Tenant.all.order('request_count DESC NULLS LAST, name ASC');
         @users = User.all
         @questions = Question.all
